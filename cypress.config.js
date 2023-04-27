f65acaf69ad762900388bd6b09042d0497360a1b
Leonardo Luiz Antunes <leoluizantunes@gmail.com>
2023-04-27 14:03:05 -0300

    fix: cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
     baseUrl: 'https://saucedemo.com',
+    screenshotOnRunFailure: true,
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
